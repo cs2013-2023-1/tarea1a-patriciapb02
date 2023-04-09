327096e11ed2077286a141f150579e4984921402
patriciapb02 <129907397+patriciapb02@users.noreply.github.com>
2023-04-09 18:13:39 -0500

    Update foo.cpp
    implementado

--- tarea/foo.cpp
+++ tarea/foo.cpp
@@ -50,8 +50,8 @@ Matriz2D::Matriz2D(const Matriz2D& m){
 
 Matriz2D::Matriz2D(Matriz2D&& m){
     // Constructor de movimiento
-    filas= move(m.filas);
-        columnas= move( m.columnas);
+    filas= std::move(m.filas);
+        columnas= std::move( m.columnas);
         ptr=new float *[filas];
         for(size_t i=0; i < filas; i++){
             ptr[i]=new float [columnas];
@@ -63,38 +63,107 @@ Matriz2D::Matriz2D(Matriz2D&& m){
 
 Matriz2D t(Matriz2D& m){
     // Transpuesta de una matriz
+    Matriz2D m1(m.columnas,m.filas);
+        for(auto i=0;i<m1.filas;i++){
+            for(auto j=0;j<m1.columnas;j++){
+                m1.ptr[i][j]=m.ptr[j][i];
+            }
+        }
+
+       return m1;
 }
 
 std::ostream& operator<<(std::ostream& os, const Matriz2D& m){
     // Sobrecarga del operador <<
+    for(size_t i=0;i<m.filas; i++){
+            for(size_t j=0;j<m.columnas; j++){
+                os << m.ptr[i][j] << " ";
+            }os<<std::endl;
+        }
+        return os;
 }
 
 Matriz2D operator+(const Matriz2D& m1, const Matriz2D& m2){
     // Sobrecarga del operador +
+    auto m3=m1;
+        if(m1.filas == m2.filas and m1.columnas == m2.columnas){
+            for(size_t i=0;i<m1.filas; i++){
+                for(size_t j=0;j<m1.columnas; j++){
+                    m3.ptr[i][j]+=m2.ptr[i][j];
+                }
+            }}
+        return m3;
 }
 
 Matriz2D operator-(const Matriz2D& m1, const Matriz2D& m2){
     // Sobrecarga del operador -
+    auto m3=m1;
+        if(m1.filas == m2.filas and m1.columnas == m2.columnas){
+            for(size_t i=0;i<m1.filas; i++){
+                for(size_t j=0;j<m1.columnas; j++){
+                    m3.ptr[i][j]-=m2.ptr[i][j];
+                }
+            }}
+        return m3;
 }
 
 Matriz2D operator*(const Matriz2D& m1, const Matriz2D& m2){
     // Sobrecarga del operador *
+    Matriz2D m3(m1.filas, m2.columnas);
+        for(size_t i=0;i<m3.filas; i++){
+            for(size_t j=0;j<m3.columnas; j++){
+                float suma{};
+                for(size_t k=0;k<m1.columnas; k++){
+                    suma+= m1.ptr[i][k] * m2.ptr[k][j];
+                }
+                m3.ptr[i][j]=suma;
+            }
+        }
+        return m3;
 }
 
 Matriz2D operator+(const Matriz2D& m, float n){
     // Sobrecarga del operador +
+    auto m2=m;
+        for(size_t i=0;i<m2.filas; i++){
+            for(size_t j=0;j<m.columnas; j++){
+                m2.ptr[i][j]+=n;
+            }
+        }
+        return m2;
 }
 
 Matriz2D operator-(const Matriz2D& m, float n){
     // Sobrecarga del operador -
+    auto m2=m;
+        for(size_t i=0;i<m2.filas; i++){
+            for(size_t j=0;j<m.columnas; j++){
+                m2.ptr[i][j]-=n;
+            }
+        }
+        return m2;
 }
 
 Matriz2D operator*(const Matriz2D& m, float n){
     // Sobrecarga del operador *
+    auto m2=m;
+        for(size_t i=0;i<m2.filas; i++){
+            for(size_t j=0;j<m.columnas; j++){
+                m2.ptr[i][j]*=n;
+            }
+        }
+        return m2;
 }
 
 Matriz2D operator/(const Matriz2D& m, float n){
     // Sobrecarga del operador /
+    auto m2=m;
+        for(size_t i=0;i<m2.filas; i++){
+            for(size_t j=0;j<m.columnas; j++){
+                m2.ptr[i][j]/=n;
+            }
+        }
+        return m2;
 }
 
 float Matriz2D::get(int i, int j){
